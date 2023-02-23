54d4941d914a23ce8e8c17ff56620cdbb7b36215
Emie0038 <Emie0038>
2023-02-23 10:21:15 +0100

    edited

--- datedecoder.py
+++ datedecoder.py
@@ -1,123 +1,51 @@
-def convert_date(date_str):
-# Split the date string into day, month, and year parts
-    day, month, year = date_str.split("-")
-
-# Convert the month abbreviation to a number
-    month_num = {
-        "JAN": "01",
-        "FEB": "02",
-        "MAR": "03",
-        "APR": "04",
-        "MAY": "05",
-        "JUN": "06",
-        "JUL": "07",
-        "AUG": "08",
-        "SEP": "09",
-        "OCT": "10",
-        "NOV": "11",
-        "DEC": "12"
-    }[month.upper()]
-
-# Combine the day, month, and year parts into a formatted date string
-    year = "20" + year if int(year) < 70 else "19" + year
-    date_formatted = f"{day}-{month_num}-{year}"
-
-    return date_formatted
 
 
-date_str = "8-MAR-85"
-converted_date_str = convert_date(date_str)
-print(converted_date_str)
-
-#Create a dict suitable for decoding month names to numbers.
-month_dict = {
-    'JAN': 1,
-    'FEB': 2,
-    'MAR': 3,
-    'APR': 4,
-    'MAY': 5,
-    'JUN': 6,
-    'JUL': 7,
-    'AUG': 8,
-    'SEP': 9,
-    'OCT': 10,
-    'NOV': 11,
-    'DEC': 12
-}
-month_name = 'MAR'
-month_num = month_dict[month_name]
-print(month_num)
-
-#Create a function which uses string operations to split the date into 3 items using the "-" character
-def split_date(date_str):
-    day, month, year = date_str.split("-")
-    return day, month, year
-
-date_str = "8-MAR-85"
-day, month, year = split_date(date_str)
-print(day)
-print(month)
-print(year)
-
-#Translate the month, correct the year to include all of the digits.
-def process_date(date_str):
-    # Split the date string into day, month, and year parts
-    day, month, year = date_str.split("-")
-
-    # Translate the month abbreviation to a number
+#4. Decode date
+def decode_date(date_str):
     month_dict = {
-        'JAN': 1,
-        'FEB': 2,
-        'MAR': 3,
-        'APR': 4,
-        'MAY': 5,
-        'JUN': 6,
-        'JUL': 7,
-        'AUG': 8,
-        'SEP': 9,
-        'OCT': 10,
-        'NOV': 11,
-        'DEC': 12
+        'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04',
+        'MAY': '05', 'JUN': '06', 'JUL': '07', 'AUG': '08',
+        'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'
     }
-    month_num = month_dict[month.upper()]
+    day, month_str, year = date_str.split('-')
+    month = month_dict[month_str.upper()]
+
+    if int(year) < 30:
+        year = '20' + year
+    else:
+        year = '19' + year
+    return (int(year), int(month), int(day))
+print(decode_date('23-FEB-23'))
+
+
+
+#Detailed with comments
+
+def decode_date(date_str):
+    # Create a dictionary to decode month names to numbers
+    month_dict = {
+        'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04',
+        'MAY': '05', 'JUN': '06', 'JUL': '07', 'AUG': '08',
+        'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'
+    }
+
+    # Split the date into day, month, and year
+    day, month, year = date_str.split('-')
 
     # Correct the year to include all of the digits
-    year = '19' + year if int(year) >= 0 and int(year) <= 21 else '20' + year
+    if int(year) < 50:
+        year = '20' + year
+    else:
+        year = '19' + year
 
-    return day, str(month_num), year
-
-date_str = "8-MAR-85"
-day, month, year = process_date(date_str)
-print(day)
-print(month)
-print(year)
-
-#The function will accept a date in the "dd-MMM-yy" format and respond with a tuple of ( y , m , d ).
-def process_date(date_str):
     # Translate the month abbreviation to a number
-    month_dict = {
-        'JAN': 1,
-        'FEB': 2,
-        'MAR': 3,
-        'APR': 4,
-        'MAY': 5,
-        'JUN': 6,
-        'JUL': 7,
-        'AUG': 8,
-        'SEP': 9,
-        'OCT': 10,
-        'NOV': 11,
-        'DEC': 12
-    }
-    day, month, year = date_str.split('-')
-    year = '20' + year if int(year) < 50 else '19' + year
-    month_num = month_dict[month.upper()]
+    month_num = month_dict[month]
 
-    return year, str(month_num), day
+    # Return the year, month, and day as a tuple
+    return year, month_num, day
 
-#call function in the string argument
-date_str = "8-MAR-85"
-year, month, day = process_date(date_str)
-print(year)
-print(month)
-print(day)
+
+#e.g
+date_str = '23-MAR-22'
+y, m, d = decode_date(date_str)
+print(y, m, d)
